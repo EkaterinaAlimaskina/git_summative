14b39c05224b578aaf43604d62cfd93bd9c86190
EkaterinaAlimaskina <katrinalimaskin@gmail.com>
2020-10-28 10:56:21 +0300

    I can write some sort algorithm

---
+++ sort.py
@@ -0,0 +1,9 @@
+def cnt_sort(lst, mx):
+    hlp = [0 for i in range(mx+1)]
+    for elem in lst:
+        hlp[elem] += 1
+    res = []
+    for i in range(mx+1):
+        res += [i for j in range(hlp[i])]
+        
+    return res
